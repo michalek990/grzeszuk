445477dfaa79b8f04c0b5caf400c01bd4184b324
michalek990 <93988101+michalek990@users.noreply.github.com>
2026-05-26 22:48:11 +0200

    fix: remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import type { Variants } from "framer-motion";
 import {
